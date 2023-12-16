9b8a61921a890d5b31a96f60442a94385012dd7c
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-16 23:11:20 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,27 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showNumber(a[0])
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    a = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.D_100_100)
+    basic.showNumber(a[0])
+})
 let a: number[] = []
 radio.setTransmitPower(7)
 radio.setFrequencyBand(1)
 radio.setGroup(220)
 qwiicjoystick.beimStart(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20))
-basic.showString("J")
-a = []
+a = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.D_100_100)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # . # .
+    # # # # #
+    . # . # .
+    `)
+let ready = true
 loops.everyInterval(500, function () {
-    a = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.D_100_100)
-    radio.sendNumber(a[0])
+    if (ready) {
+        a = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.D_100_100)
+        radio.sendNumber(a[0])
+    }
 })
