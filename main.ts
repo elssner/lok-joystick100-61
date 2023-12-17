92d5d6b937b229b5b00c28c12e7cc287e06b4492
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-17 14:33:04 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showNumber(a[0])
+    live = true
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    a = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.D_100_100)
     basic.showNumber(a[0])
 })
+input.onButtonEvent(Button.A, input.buttonEventValue(ButtonEvent.Hold), function () {
+    live = false
+})
+let live = false
 let a: number[] = []
 radio.setTransmitPower(7)
 radio.setFrequencyBand(1)
@@ -18,10 +21,13 @@ basic.showLeds(`
     # # # # #
     . # . # .
     `)
+live = true
 let ready = true
 loops.everyInterval(500, function () {
     if (ready) {
-        a = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.D_100_100)
+        if (live) {
+            a = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.D_100_100)
+        }
         radio.sendNumber(a[0])
     }
 })
